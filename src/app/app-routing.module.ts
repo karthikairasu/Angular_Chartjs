d60764fcccade76c8c15d2673a0ae130c468ee03
karthikairasu <karthikairasuk@gmail.com>
2022-08-28 00:34:57 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChartjsComponent } from './chartjs/chartjs.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'chartjs', pathMatch: 'full' },
+  { path: 'chartjs', component: ChartjsComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
